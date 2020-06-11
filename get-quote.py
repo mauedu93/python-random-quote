5c3735cce4e4cdfbeb2c28ec45ebf36e16b4635c
Mauricio Castro <63480061+mauedu93@users.noreply.github.com>
2020-06-11 15:48:21 -0700

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,9 +1,13 @@
 def primary():
   # print("Keep it logically awesome.")
 
+ with open("quotes.txt", 'a') as f:
+     f.write("Ain't no mountain high enough\n")
+
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
+
 
   print(quotes[0])
 
