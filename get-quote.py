4332e27fe6ddf7b5d339969d9baf80f400ff6952
Mauricio Castro <63480061+mauedu93@users.noreply.github.com>
2020-06-12 21:53:36 -0700

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,18 +1,18 @@
 def primary():
   # print("Keep it logically awesome.")
   #
-  # with open("quotes.txt", 'a') as f:
-  #    f.write("Ain't no mountain high enough\n")
+  with open("quotes.txt", 'a') as f:
+     f.write("Another bites the dust\n")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+  # f = open("quotes.txt")
+  # quotes = f.readlines()
+  # f.close()
 
   # print(quotes[-1][:-1])
 #
 #
-  for n in range(10):
-      print(quotes[n][:-1])
+  # for n in range(10):
+  #     print(quotes[n][:-1])
 #
 if __name__== "__main__":
   primary()
